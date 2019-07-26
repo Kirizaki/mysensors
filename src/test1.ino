b63d5747300d41336a4ea9580e737447dea8d5b1
th0m4s <th0m4s@o2.pl>
2019-07-26 21:16:40 +0200

    switch to OneButton lib

--- src/test1.ino
+++ src/test1.ino
@@ -38,19 +38,17 @@
 //#define MY_DEFAULT_TX_LED_PIN  5  // the PCB, on board LED
 
 #include <MySensors.h>  
-#include <Bounce2.h>
-
+#include "OneButton.h"
 // Enable repeater functionality for this node
 #define MY_REPEATER_FEATURE
 
 
-#define RELAY_1  4  // Arduino Digital I/O pin number for first relay (second on pin+1 etc)
-#define NUMBER_OF_RELAYS 1 // Total number of attached relays
+#define RELAY_1  26  // Arduino Digital I/O pin number for first relay (second on pin+1 etc)
+#define NUMBER_OF_RELAYS 13 // Total number of attached relays
 #define RELAY_ON 1  // GPIO value to write to turn on attached relay
 #define RELAY_OFF 0 // GPIO value to write to turn off attached relay
 
-#define BUTTON_PIN A1
-
+OneButton button1(A1, true);
 
 void before() { 
   for (int sensor=1, pin=RELAY_1; sensor<=NUMBER_OF_RELAYS;sensor++, pin++) {
@@ -60,16 +58,10 @@ void before() {
     digitalWrite(pin, loadState(sensor)?RELAY_ON:RELAY_OFF);
   }
 }
-Bounce debouncer = Bounce();
 
 void setup() { 
-  // Setup locally attached sensors
-  delay(10000);
    // Setup the button.
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
-  // After setting up the button, setup debouncer.
-  debouncer.attach(BUTTON_PIN);
-  debouncer.interval(5);
+  button1.attachClick(click1);
   //presentation();
 }
 void presentation()  
@@ -86,20 +78,15 @@ void presentation()
 MyMessage msg(1, V_LIGHT);
 
 void loop() { 
-  // Send locally attached sensor data here 
-  if (debouncer.update()) {
-    // Get the update value.
-    int value = debouncer.read();
-    // Send in the new value.
-    if(value == LOW){
-         saveState(1, !loadState(1));
-         digitalWrite(RELAY_1, loadState(1)?RELAY_ON:RELAY_OFF);
-         send(msg.set(loadState(1)));
-    }
-  }
+  // keep sensing buttons
+  button1.tick();
 }
-
-
+void click1() {
+  saveState(1, !loadState(1));
+  digitalWrite(RELAY_1, loadState(1)?RELAY_ON:RELAY_OFF);
+         send(msg.set(loadState(1)));
+} // click1
+ 
 void receive(const MyMessage &message) {
   // We only expect one type of message from controller. But we better check anyway.
   if (message.type==V_LIGHT) {
